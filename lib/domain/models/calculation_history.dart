b6a8e725b250f7cb128270eb33f051fbfe5d3287
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-09-24 21:34:45 +0700

    feat(models): calculation history model

---
+++ lib/domain/models/calculation_history.dart
@@ -0,0 +1,7 @@
+class CalculationHistory {
+  final double weight;
+  final double speed;
+  final double kineticEnergy;
+
+  CalculationHistory({required this.weight, required this.speed, required this.kineticEnergy});
+}
