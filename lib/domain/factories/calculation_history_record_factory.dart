f458123ff35f22ccf1626632a9998d5f1394a3f1
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-09-25 17:01:55 +0700

    feat(factories):

---
+++ lib/domain/factories/calculation_history_record_factory.dart
@@ -0,0 +1,7 @@
+import 'package:edu_flutter_app/domain/models/calculation_history_record.dart';
+
+class CaclulationHistoryRecordFactory {
+  static CalculationHistoryRecord make(double weight, double speed, double kineticEnergy) {
+    return (weight: weight, speed: speed, kineticEnergy: kineticEnergy);
+  }
+}
---
+++ lib/domain/factories/calculation_input_factory.dart
@@ -0,0 +1,14 @@
+import 'package:edu_flutter_app/domain/models/calculation_input.dart';
+
+class CalculationInputFactory {
+  static CalculationInput? make(String weight, String speed) {
+    var parsedWeight = double.tryParse(weight);
+    var parsedSpeed = double.tryParse(speed);
+
+    if (parsedWeight == null || parsedSpeed == null) {
+      return null;
+    }
+
+    return (weight: parsedWeight, speed: parsedSpeed);
+  }
+}
