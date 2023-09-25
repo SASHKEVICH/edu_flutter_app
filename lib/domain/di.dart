5894df2cf15e0fa48030e0c3dd4c857fc48cca13
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-09-25 13:52:59 +0700

    feat(): registrating dbservice in di

--- lib/domain/di.dart
+++ lib/domain/di.dart
@@ -1,9 +1,15 @@
+import 'package:edu_flutter_app/domain/database/db_service.dart';
 import 'package:edu_flutter_app/domain/kinetic_calculator/kinetic_calculator_service.dart';
 
 class Di {
   final KineticCalculatorService _kineticCalculatorService = KineticCalculatorService();
+  final DBService _dbService = DBService();
 
   KineticCalculatorService getKineticCalculatorService() {
     return _kineticCalculatorService;
+  }
+
+  DBService getDbService() {
+    return _dbService;
   }
 }
