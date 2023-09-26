52cc698832f5ece645894e892ca94a71c8fb9673
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-09-26 14:31:11 +0700

    feat(): nasa photos page

---
+++ lib/scenes/nasa_photos/nasa_photos_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:edu_flutter_app/scenes/nasa_photos/cubit/nasa_photos_screen_cubit.dart';
+import 'package:edu_flutter_app/scenes/nasa_photos/cubit/nasa_photos_screen_state.dart';
+import 'package:edu_flutter_app/scenes/nasa_photos/nasa_photos_screen.dart';
+import 'package:edu_flutter_app/domain/models/nasa/nasa_photo.dart';
+
+class NasaPhotosPage extends StatefulWidget {
+  const NasaPhotosPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _NasaPhotosPageState();
+}
+
+class _NasaPhotosPageState extends State<NasaPhotosPage> {
+  List<NasaPhoto> _photos = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BlocBuilder<NasaPhotosScreenCubit, NasaPhotosScreenState>(
+        builder: (context, state) {
+          if (state is LoadingState) {
+            BlocProvider.of<NasaPhotosScreenCubit>(context).loadPhotos();
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (state is LoadedState) {
+            _photos += state.photos;
+            return NasaPhotosScreen(photos: _photos);
+          } else if (state is ErrorState) {
+            return Center(
+              child: Column(
+                children: [
+                  const Text("Something went wrong"),
+                  Text(state.errorMessage),
+                  ElevatedButton(
+                    onPressed: () {
+                      BlocProvider.of<NasaPhotosScreenCubit>(context).loadPhotos();
+                    }, 
+                    child: const Text("Try again")
+                  )
+                ]
+              ),
+            );
+          } else { 
+            return Container();
+          }
+        },
+      )
+    );
+  }
+}
