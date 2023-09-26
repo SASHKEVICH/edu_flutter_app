6091337fc9b702527a4a21ed67c4623b612718d6
Aleksandr Bekrenev <praise2001@yandex.ru>
2023-09-26 15:17:57 +0700

    feat(nasa-photos-screen): infinite list & pagination

--- lib/scenes/nasa_photos/nasa_photos_screen.dart
+++ lib/scenes/nasa_photos/nasa_photos_screen.dart
@@ -1,5 +1,7 @@
 import 'package:edu_flutter_app/domain/models/nasa/nasa_photo.dart';
+import 'package:edu_flutter_app/scenes/nasa_photos/cubit/nasa_photos_screen_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class NasaPhotosScreen extends StatefulWidget {
   const NasaPhotosScreen({super.key, required this.photos});
@@ -11,6 +13,8 @@ class NasaPhotosScreen extends StatefulWidget {
 }
 
 class _NasaPhotosScreenState extends State<NasaPhotosScreen> {
+  final int _nextPageTrigger = 20;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +26,20 @@ class _NasaPhotosScreenState extends State<NasaPhotosScreen> {
         itemCount: widget.photos.length,
         itemBuilder: (BuildContext context, int index) {
           var item = widget.photos[index];
+
+          if (index == widget.photos.length - _nextPageTrigger) {
+            BlocProvider.of<NasaPhotosScreenCubit>(context).loadPhotos();
+          }
+
+          if (index == widget.photos.length) {
+            return const Center(
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: CircularProgressIndicator(),
+              )
+            );
+          }
+
           return ListTile(
             title: Text(item.earthDate ?? ''),
           );
